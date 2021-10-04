97073fe74b74fb1b5aa1377bdc2118d560c5ace8
justinweng <justinweng@google.com>
2021-10-05 02:01:27 +0800

    add controls for BT LC3 codec
    Signed-off-by: justinweng <justinweng@google.com>
    Change-Id: Ib73a4cee5c15b8878caa7f3d20ad2f85739b2f4b

--- alsa/aoc_alsa.h
+++ alsa/aoc_alsa.h
@@ -110,6 +110,8 @@ enum bluetooth_mode {
 	AHS_BT_MODE_A2DP_RAW,
 	AHS_BT_MODE_A2DP_ENC_SBC,
 	AHS_BT_MODE_A2DP_ENC_AAC,
+	AHS_BT_MODE_A2DP_ENC_LC3,
+	AHS_BT_MODE_BLE_ENC_LC3,
 };
 
 enum TelephonyModes {
