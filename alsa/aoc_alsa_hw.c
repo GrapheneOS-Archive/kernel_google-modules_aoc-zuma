freak07/Kirisakura_Raviole

commit 2742a8c6eb2768aebebab482f71601bba495ada7

aoc/alsa: change pcm callbacks to be nonatomic
Pcm callbacks such as triggers are atomic by default. Since the mic
input start cmd sent to AoC takes more than 50 ms, the spin-lock being
used will disable preemption for more than 50ms. So we choose to use
mutex to allow preemption.

As a result, all the pcm callbacks should be nonatomic and
snd_pcm_period_elapsed() should be called within the process
context.

Changes made here are:

- Set pcm->nonatomic as true
- Use mutex instead of spin_lock for aoc_audio_control()
- Use workqueue to update the pcm hw_ptr

Bug: 196817394
Change-Id: Ibc858555b85dedd2560f92cd15e1d059c61f9fce

alsa/aoc_alsa_hw.c
@@ -161,18 +161,13 @@ static int aoc_audio_control(const char *cmd_channel, const uint8_t *cmd,
 
 	in_pcm_trigger_callback = irqs_disabled();
 
-	if (in_pcm_trigger_callback)
-		spin_lock(&chip->audio_lock);
-	else
-		spin_lock_bh(&chip->audio_lock);
+	if (mutex_lock_interruptible(&chip->audio_cmd_chan_mutex))
+		return -EINTR;
 
 	/* Get the aoc audio control channel at runtime */
 	err = alloc_aoc_audio_service(cmd_channel, &dev, NULL, NULL);
 	if (err < 0) {
-		if (in_pcm_trigger_callback)
-			spin_unlock(&chip->audio_lock);
-		else
-			spin_unlock_bh(&chip->audio_lock);
+		mutex_unlock(&chip->audio_cmd_chan_mutex);
 		return err;
 	}
 
@@ -259,10 +254,7 @@ exit:
 	kfree(buffer);
 	free_aoc_audio_service(cmd_channel, dev);
 
-	if (in_pcm_trigger_callback)
-		spin_unlock(&chip->audio_lock);
-	else
-		spin_unlock_bh(&chip->audio_lock);
+	mutex_unlock(&chip->audio_cmd_chan_mutex);
 
 	return err < 1 ? -EAGAIN : 0;
 }

alsa/aoc_alsa.h
@@ -218,6 +218,7 @@ struct aoc_chip {
 	unsigned int opened;
 	unsigned int capture_param_set;
 	struct mutex audio_mutex;
+	struct mutex audio_cmd_chan_mutex;
 	spinlock_t audio_lock;
 	long pcm_wait_time_in_ms;
 	long voice_pcm_wait_time_in_ms;
@@ -262,12 +263,14 @@ struct aoc_alsa_stream {
 	int draining;
 
 	struct work_struct free_aoc_service_work;
+	struct work_struct pcm_period_work;
 };
 
 void aoc_timer_start(struct aoc_alsa_stream *alsa_stream);
 void aoc_timer_restart(struct aoc_alsa_stream *alsa_stream);
 void aoc_timer_stop(struct aoc_alsa_stream *alsa_stream);
 void aoc_timer_stop_sync(struct aoc_alsa_stream *alsa_stream);
+void aoc_pcm_period_work_handler(struct work_struct *work);
 
 int snd_aoc_new_ctl(struct aoc_chip *chip);
 int snd_aoc_new_pcm(struct aoc_chip *chip);

alsa/aoc_alsa_card.c
@@ -1837,6 +1837,7 @@ static int snd_aoc_init(struct aoc_chip *chip)
 	chip->mute = 1;
 
 	mutex_init(&chip->audio_mutex);
+	mutex_init(&chip->audio_cmd_chan_mutex);
 	spin_lock_init(&chip->audio_lock);
 
 	return 0;

alsa/aoc_alsa_incall.c
@@ -88,7 +88,7 @@ static enum hrtimer_restart aoc_pcm_hrtimer_irq_handler(struct hrtimer *timer)
 		alsa_stream->pos = (consumed - alsa_stream->hw_ptr_base) % alsa_stream->buffer_size;
 	}
 
-	snd_pcm_period_elapsed(alsa_stream->substream);
+	schedule_work(&alsa_stream->pcm_period_work);
 
 	return HRTIMER_RESTART;
 }
@@ -148,6 +148,7 @@ static int snd_aoc_pcm_open(struct snd_soc_component *component,
 	alsa_stream->dev = dev;
 	alsa_stream->idx = idx;
 	alsa_stream->stream_type = aoc_pcm_device_to_stream_type(idx);
+	INIT_WORK(&alsa_stream->pcm_period_work, aoc_pcm_period_work_handler);
 
 	/* Ring buffer will be flushed at prepare() before playback/capture */
 	alsa_stream->hw_ptr_base = (substream->stream == SNDRV_PCM_STREAM_PLAYBACK) ?
@@ -202,6 +203,7 @@ static int snd_aoc_pcm_close(struct snd_soc_component *component,
 
 	dev_dbg(component->dev, "name %s substream %pK", rtd->dai_link->name, substream);
 	aoc_timer_stop_sync(alsa_stream);
+	cancel_work_sync(&alsa_stream->pcm_period_work);
 
 	if (mutex_lock_interruptible(&chip->audio_mutex)) {
 		dev_err(component->dev, "ERR: interrupted while waiting for lock\n");
@@ -487,6 +489,7 @@ static int aoc_pcm_new(struct snd_soc_component *component, struct snd_soc_pcm_r
 					      snd_aoc_playback_hw.buffer_bytes_max);
 	}
 
+	rtd->pcm->nonatomic = true;
 	return 0;
 }
 

alsa/aoc_alsa_pcm.c
@@ -33,6 +33,7 @@ static void free_aoc_service_work_handler(struct work_struct *work)
 		return;
 
 	aoc_timer_stop_sync(alsa_stream);
+	cancel_work_sync(&alsa_stream->pcm_period_work);
 
 	if (mutex_lock_interruptible(&chip->audio_mutex)) {
 		pr_err("ERR: interrupted while waiting for lock\n");
@@ -45,6 +46,16 @@ static void free_aoc_service_work_handler(struct work_struct *work)
 	alsa_stream->dev = NULL;
 
 	mutex_unlock(&chip->audio_mutex);
+	return;
+}
+
+void aoc_pcm_period_work_handler(struct work_struct *work)
+{
+	struct aoc_alsa_stream *alsa_stream =
+		container_of(work, struct aoc_alsa_stream, pcm_period_work);
+
+	snd_pcm_period_elapsed(alsa_stream->substream);
+
 	return;
 }
 
@@ -189,7 +200,7 @@ static enum hrtimer_restart aoc_pcm_hrtimer_irq_handler(struct hrtimer *timer)
 		alsa_stream->pos = (consumed - alsa_stream->hw_ptr_base) % alsa_stream->buffer_size;
 	}
 
-	snd_pcm_period_elapsed(alsa_stream->substream);
+	schedule_work(&alsa_stream->pcm_period_work);
 
 	return HRTIMER_RESTART;
 }
@@ -242,6 +253,7 @@ static int snd_aoc_pcm_open(struct snd_soc_component *component,
 	alsa_stream->stream_type = aoc_pcm_device_to_stream_type(idx);
 
 	INIT_WORK(&alsa_stream->free_aoc_service_work, free_aoc_service_work_handler);
+	INIT_WORK(&alsa_stream->pcm_period_work, aoc_pcm_period_work_handler);
 
 	/* Find the corresponding aoc audio service */
 	err = alloc_aoc_audio_service(rtd->dai_link->name, &dev, aoc_pcm_reset_handler,
@@ -276,6 +288,8 @@ static int snd_aoc_pcm_open(struct snd_soc_component *component,
 	hrtimer_init(&(alsa_stream->hr_timer), CLOCK_MONOTONIC, HRTIMER_MODE_REL);
 	alsa_stream->hr_timer.function = &aoc_pcm_hrtimer_irq_handler;
 
+	pr_debug("rtd->pcm->nonatomic = %d\n", rtd->pcm->nonatomic);
+
 	/* TODO: refactor needed on mapping between device number and entrypoint */
 	alsa_stream->entry_point_idx = (idx == 7) ? HAPTICS : idx;
 	mutex_unlock(&chip->audio_mutex);
@@ -291,6 +305,7 @@ out:
 
 	if (alsa_stream) {
 		cancel_work_sync(&alsa_stream->free_aoc_service_work);
+		cancel_work_sync(&alsa_stream->pcm_period_work);
 		kfree(alsa_stream);
 	}
 
@@ -310,6 +325,7 @@ static int snd_aoc_pcm_close(struct snd_soc_component *component,
 
 	pr_debug("%s: name %s substream %p", __func__, rtd->dai_link->name, substream);
 	aoc_timer_stop_sync(alsa_stream);
+	cancel_work_sync(&alsa_stream->pcm_period_work);
 
 	if (mutex_lock_interruptible(&chip->audio_mutex)) {
 		pr_err("ERR: interrupted while waiting for lock\n");
@@ -687,6 +703,8 @@ static int aoc_pcm_new(struct snd_soc_component *component, struct snd_soc_pcm_r
 	if (!rtd->ops.ack) {
 		rtd->ops.ack = snd_aoc_pcm_ack;
 	}
+
+	rtd->pcm->nonatomic = true;
 
 	return 0;
 }

alsa/aoc_alsa_voice.c
@@ -314,6 +314,7 @@ static int aoc_pcm_new(struct snd_soc_component *component, struct snd_soc_pcm_r
 			snd_aoc_playback_hw.buffer_bytes_max);
 	}
 
+	rtd->pcm->nonatomic = true;
 	return 0;
 }
 

alsa/aoc_alsa_voip.c
@@ -87,7 +87,7 @@ static enum hrtimer_restart aoc_pcm_hrtimer_irq_handler(struct hrtimer *timer)
 		alsa_stream->pos = (consumed - alsa_stream->hw_ptr_base) % alsa_stream->buffer_size;
 	}
 
-	snd_pcm_period_elapsed(alsa_stream->substream);
+	schedule_work(&alsa_stream->pcm_period_work);
 
 	return HRTIMER_RESTART;
 }
@@ -146,6 +146,7 @@ static int snd_aoc_pcm_open(struct snd_soc_component *component,
 	alsa_stream->cstream = NULL;
 	alsa_stream->dev = dev;
 	alsa_stream->idx = idx;
+	INIT_WORK(&alsa_stream->pcm_period_work, aoc_pcm_period_work_handler);
 
 	/* Ring buffer will be flushed at prepare() before playback/capture */
 	alsa_stream->hw_ptr_base = (substream->stream == SNDRV_PCM_STREAM_PLAYBACK) ?
@@ -200,6 +201,7 @@ static int snd_aoc_pcm_close(struct snd_soc_component *component,
 
 	dev_dbg(component->dev, "name %s substream %pK", rtd->dai_link->name, substream);
 	aoc_timer_stop_sync(alsa_stream);
+	cancel_work_sync(&alsa_stream->pcm_period_work);
 
 	if (mutex_lock_interruptible(&chip->audio_mutex)) {
 		dev_err(component->dev, "ERR: interrupted while waiting for lock\n");
@@ -244,7 +246,6 @@ static int snd_aoc_pcm_close(struct snd_soc_component *component,
 	chip->opened &= ~(1 << alsa_stream->idx);
 
 	mutex_unlock(&chip->audio_mutex);
-
 	return 0;
 }
 
@@ -478,6 +479,7 @@ static int aoc_pcm_new(struct snd_soc_component *component, struct snd_soc_pcm_r
 					      snd_aoc_playback_hw.buffer_bytes_max);
 	}
 
+	rtd->pcm->nonatomic = true;
 	return 0;
 }
 

GrapheneOS-Archive/kernel_google-modules_aoc-zuma

commit 7d50b48bef753e07926438320431e579e1c358ab

aoc/alsa: Remove call to irqs_disabled()
Since this isn't used anymore, remove it. It's causing a Werror failure
due to setting an unused variable.

Fixes: 2742a8c6eb27 ("aoc/alsa: change pcm callbacks to be nonatomic")
Signed-off-by: Will McVicker <willmcvicker@google.com>
Change-Id: Ia8385f13ca533f7312fe708339b6bc7a02a881b2

alsa/aoc_alsa_hw.c
@@ -154,12 +154,9 @@ static int aoc_audio_control(const char *cmd_channel, const uint8_t *cmd,
 	struct timespec64 tv0, tv1;
 	int err, count;
 	unsigned long time_expired;
-	int in_pcm_trigger_callback;
 
 	if (!cmd_channel || !cmd)
 		return -EINVAL;
-
-	in_pcm_trigger_callback = irqs_disabled();
 
 	if (mutex_lock_interruptible(&chip->audio_cmd_chan_mutex))
 		return -EINTR;