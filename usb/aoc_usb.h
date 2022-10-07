aee38e3d450fa5402108e00cfec0bc903e4e9d3c
Albert Wang <albertccwang@google.com>
2022-10-07 10:02:26 +0800

    aoc: usb: separate the driver interface and implementation
    Refactor the aoc usb driver to separate the interface and
    implementation.
    
    Bug: 242172060
    Test: local build pass and boot to home
    Signed-off-by: Albert Wang <albertccwang@google.com>
    Change-Id: I27b9a712cf52cfeeae1bc5139ba9cfbe388f96b0

--- usb/aoc_usb.h
+++ usb/aoc_usb.h
@@ -13,20 +13,10 @@
 
 #include "xhci.h"
 
-/*
- * This variable used to present if aoc_usb module was probed done. If offload
- * is enabled, the controller needs to wait for the aoc_usb probe done and then
- * continue the controller's probe.
- */
-extern bool aoc_usb_probe_done;
-
 enum aoc_usb_msg {
-	SYNC_DEVICE_CONTEXT,
-	GET_DCBAA_PTR,
 	SET_DCBAA_PTR,
 	GET_TR_DEQUEUE_PTR,
 	SETUP_DONE,
-	GET_ISOC_TR_INFO,
 	SET_ISOC_TR_INFO,
 	SYNC_CONN_STAT,
 	SET_OFFLOAD_STATE
@@ -37,41 +27,11 @@ enum aoc_usb_state {
 	USB_CONNECTED
 };
 
-enum usb_offload_op_mode {
-	USB_OFFLOAD_STOP,
-	USB_OFFLOAD_SIMPLE_AUDIO_ACCESSORY,
-	USB_OFFLOAD_DRAM
-};
-
 enum usb_recover_state {
 	NONE,
 	RECOVER_HOST_OFF,
 	RECOVER_HOST_ON,
 	RECOVERED
-};
-
-enum usb_dev_hub {
-	UNDEFINED,
-	USB1,
-	USB1_1,
-	USB2,
-	USB2_1,
-	USB3,
-	USB3_1
-};
-
-struct xhci_vendor_data {
-	struct xhci_hcd *xhci;
-
-	bool usb_accessory_enabled;
-	bool usb_audio_offload;
-	bool dt_direct_usb_access;
-	bool offload_state;
-
-	enum usb_offload_op_mode op_mode;
-
-	struct workqueue_struct *irq_wq;
-	struct work_struct xhci_vendor_irq_work;
 };
 
 struct aoc_usb_drvdata {
@@ -84,12 +44,6 @@ struct aoc_usb_drvdata {
 
 	long service_timeout;
 	unsigned int usb_conn_state;
-};
-
-struct get_dev_ctx_args {
-	unsigned int slot_id;
-	size_t length;
-	u8 *dev_ctx;
 };
 
 struct conn_stat_args {
@@ -112,25 +66,19 @@ struct get_isoc_tr_info_args {
 	u32 num_trbs_free;
 };
 
-int xhci_vendor_helper_init(void);
-int usb_vendor_helper_init(void);
-int snd_usb_audio_vendor_helper_init(void);
-
-extern int xhci_handle_event(struct xhci_hcd *xhci);
-extern void xhci_update_erst_dequeue(struct xhci_hcd *xhci,
-				     union xhci_trb *event_ring_deq);
-extern int xhci_exynos_register_vendor_ops(struct xhci_vendor_ops *vendor_ops);
-int xhci_set_offload_state(struct xhci_hcd *xhci, bool enabled);
-struct xhci_hcd *get_xhci_hcd_by_udev(struct usb_device *udev);
-
-int usb_host_mode_state_notify(enum aoc_usb_state usb_state);
-
 int register_aoc_usb_notifier(struct notifier_block *nb);
 int unregister_aoc_usb_notifier(struct notifier_block *nb);
 
-extern int dwc3_otg_host_enable(bool enabled);
+int notify_offload_state(bool enabled);
+int xhci_set_dcbaa_ptr(u64 aoc_dcbaa_ptr);
+int xhci_setup_done(void);
+int xhci_sync_conn_stat(unsigned int bus_id, unsigned int dev_num, unsigned int slot_id,
+			       unsigned int conn_stat);
+int usb_host_mode_state_notify(enum aoc_usb_state usb_state);
+int xhci_set_isoc_tr_info(u16 ep_id, u16 dir, struct xhci_ring *ep_ring);
 
-extern bool aoc_alsa_usb_capture_enabled(void);
-extern bool aoc_alsa_usb_playback_enabled(void);
+bool is_aoc_usb_probe_done(void);
+
+extern int dwc3_otg_host_enable(bool enabled);
 
 #endif /* __LINUX_AOC_USB_H */
